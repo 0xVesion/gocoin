d48d47b9e0dce756a7b1cfe6f9649d6fd1e33ca3
Vincent Engel <ve@vesion.io>
2021-10-31 01:38:44 +0200

    Implemented miner client

---
+++ cmd/miner/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"vesion.io/go/gocoin/core"
+	"vesion.io/go/gocoin/miner"
+	"vesion.io/go/gocoin/pb"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client := pb.NewNodeClient(conn)
+
+	tasks, err := client.StreamTasks(context.Background(), &emptypb.Empty{})
+	if err != nil {
+		panic(err)
+	}
+
+	onCancel := make(chan interface{})
+	onResult := make(chan string)
+
+	for {
+		task, err := tasks.Recv()
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("Received new task. difficulty: %v\n", task.Difficulty)
+
+		close(onCancel)
+		onCancel = make(chan interface{})
+		onResult = make(chan string)
+		go miner.Mine(onCancel, &core.Block{
+			Parent:    task.GetParent(),
+			Timestamp: task.GetTimestamp(),
+			Data:      task.GetData(),
+		}, int(task.GetDifficulty()), onResult)
+		go func() {
+			for {
+				select {
+				case <-onCancel:
+					log.Println("Cancelling receiver goroutine")
+					return
+				case nonce := <-onResult:
+					_, err := client.SubmitTask(context.Background(), &pb.Submission{Nonce: nonce})
+					if err == nil {
+						fmt.Println("WON!")
+						return
+					}
+					log.Println(err)
+				}
+			}
+		}()
+	}
+}
---
+++ miner/miner.go
@@ -0,0 +1,42 @@
+package miner
+
+import (
+	"log"
+	"time"
+
+	"vesion.io/go/gocoin/core"
+)
+
+func Mine(onCancel chan interface{}, block *core.Block, difficulty int, result chan string) {
+	var seconds int
+	var tries int
+
+	go func() {
+		for {
+			select {
+			case <-onCancel:
+				log.Println("Cancelling stats goroutine")
+				return
+			case <-time.After(time.Second):
+				seconds++
+				log.Printf("Mining %v @ %vkh/s\n", block.Parent[:4], tries/seconds/1000)
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-onCancel:
+			log.Println("Cancelling miner goroutine")
+			return
+		default:
+			tries++
+			block.Nonce = core.GenerateNonce()
+			block.Hash = block.CalcHash()
+
+			if block.Difficulty() >= difficulty {
+				result <- block.Nonce
+			}
+		}
+	}
+}
